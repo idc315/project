d51527286c4dda2c93b17521473893c47607fae0
idc315 <hoangminhhieu315@gmail.com>
2024-06-20 14:40:45 +0900

    Update new 3.js

--- new 3.js
+++ new 3.js
@@ -1,3 +1,29 @@
+let slideIndex = 0;
+const slides = document.getElementsByClassName("slide");
+const totalSlides = 9;
+
+function showSlide(n) {
+  for (let i = 0; i < 9; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex += n;
+  if (slideIndex >= totalSlides) {
+    slideIndex = 0;
+  }
+  if (slideIndex < 0) {
+    slideIndex = totalSlides - 1;
+  }
+  
+  slides[slideIndex].style.display = "block";
+}
+function moveSlides(n) {
+  showSlide(n);
+}
+
+setInterval(() => {
+  showSlide(1);
+}, 3000);
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const navToggle = document.getElementById('nav-toggle');
     const navBar = document.getElementById('nav-bar');
